df4020144178e1bb44b2e7646dc5c63b517f7f81
kirill <kirill.grim@gmail.com>
2021-07-01 13:18:41 +0300

    Moved init at the top of Configuration class

--- config.py
+++ config.py
@@ -23,6 +23,10 @@ import json
 
 class Configuration:
 
+    def __init__(self, file_path: str):
+        with open(os.path.expanduser(file_path)) as input_file:
+            self._config_data = json.load(input_file)
+
     @property
     def config_data(self) -> dict:
         return self._config_data
@@ -32,7 +36,3 @@ class Configuration:
         if not isinstance(value, dict):
             raise TypeError(f'Type: {type(value)} not supported')
         self._config_data = value
-
-    def __init__(self, file_path: str):
-        with open(os.path.expanduser(file_path)) as input_file:
-            self._config_data = json.load(input_file)
