38d024f5fe72bbad4b6d5a4b88ea84f24bb0181b
kirill <kirill.grim@gmail.com>
2021-07-26 19:18:21 +0300

    Updated configs to parse server config

--- chat_server/config.py
+++ chat_server/config.py
@@ -1,3 +1,4 @@
 config_file_path = os.environ.get('CHAT_SERVER_CONFIG', '../config.json')
+server_env = os.environ.get('SERVER_ENV', 'LOCALHOST')
 
-app_config = Configuration(file_path=config_file_path).config_data
+app_config = Configuration(file_path=config_file_path).config_data.get('CHAT_SERVER', {}).get('SERVER_ENV', 'LOCALHOST')
