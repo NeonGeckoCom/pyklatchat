8100e93d43e2b8790d808ac38b7a2ef6efeed795
kirill <kirill.grim@gmail.com>
2021-11-22 18:16:42 +0200

    small fix

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -2,9 +2,9 @@ cd ..
 echo "Building Chat Observer"
 CP /utils/common.py /services/klatchat_observer/utils
 CP /requirements/requirements.txt /services/klatchat_observer
-docker build -f dockerfiles/Dockerfile.observer -t neon/klatchat_observer:0.0.2 services/klatchat_observer
+docker build -f dockerfiles/Dockerfile.observer -t neon/klatchat_observer:latest services/klatchat_observer
 echo "Building Chat Client"
-docker build -f dockerfiles/Dockerfile.client -t neon/chat_client:0.0.2 chat_client
+docker build -f dockerfiles/Dockerfile.client -t neon/chat_client:latest chat_client
 echo "Building Chat Server"
-docker build -f dockerfiles/Dockerfile.server -t neon/chat_server:0.0.2 chat_server
+docker build -f dockerfiles/Dockerfile.server -t neon/chat_server:latest chat_server
 cd scripts
