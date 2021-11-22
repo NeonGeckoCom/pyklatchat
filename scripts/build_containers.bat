e4a58cefc790b9155428a0e5aae6714d7f82915d
kirill <kirill.grim@gmail.com>
2021-11-22 18:15:05 +0200

    Refactored scripts, made unified docker structure

---
+++ scripts/build_containers.bat
@@ -0,0 +1,10 @@
+cd ..
+echo "Building Chat Observer"
+COPY "\utils\common.py" "\services\klatchat_observer\utils"
+COPY "\requirements\requirements.txt" "\services\klatchat_observer"
+docker build -f dockerfiles/Dockerfile.observer -t neon/klatchat_observer:0.0.2 services/klatchat_observer
+echo "Building Chat Client"
+docker build -f dockerfiles/Dockerfile.client -t neon/chat_client:0.0.2 chat_client
+echo "Building Chat Server"
+docker build -f dockerfiles/Dockerfile.server -t neon/chat_server:0.0.2 chat_server
+cd scripts
