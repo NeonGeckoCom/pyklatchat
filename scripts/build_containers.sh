60b369bf61509728c2b10644bdc99365703068ce
kirill <kirill.grim@gmail.com>
2021-11-22 19:17:40 +0200

    Refactored dockerfiles and build script

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -4,8 +4,12 @@ cp utils/common.py services/klatchat_observer/utils
 cp requirements/requirements.txt services/klatchat_observer
 cd services/klatchat_observer
 docker build -f ../../dockerfiles/Dockerfile.observer -t neon/klatchat_observer:latest .
-# echo "Building Chat Client"
-# docker build -f dockerfiles/Dockerfile.client -t neon/chat_client:latest chat_client
-# echo "Building Chat Server"
-# docker build -f dockerfiles/Dockerfile.server -t neon/chat_server:latest chat_server
 cd ../../scripts
+echo "Building Chat Client"
+cd chat_client
+docker build -f ../dockerfiles/Dockerfile.client -t neon/chat_client:latest chat_client
+cd ../scripts
+echo "Building Chat Server"
+cd chat_server
+docker build -f ../dockerfiles/Dockerfile.server -t neon/chat_server:latest chat_server
+cd ../scripts
