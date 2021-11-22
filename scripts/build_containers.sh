b86effe0f19ff8b811111fb7d2dc7999f0ff0432
root <guy@neongecko.com>
2021-11-22 09:08:18 -0800

    Refactored scripts

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -1,10 +1,11 @@
 cd ..
 echo "Building Chat Observer"
-CP /utils/common.py /services/klatchat_observer/utils
-CP /requirements/requirements.txt /services/klatchat_observer
-docker build -f dockerfiles/Dockerfile.observer -t neon/klatchat_observer:latest services/klatchat_observer
-echo "Building Chat Client"
-docker build -f dockerfiles/Dockerfile.client -t neon/chat_client:latest chat_client
-echo "Building Chat Server"
-docker build -f dockerfiles/Dockerfile.server -t neon/chat_server:latest chat_server
-cd scripts
+cp utils/common.py services/klatchat_observer/utils
+cp requirements/requirements.txt services/klatchat_observer
+cd services/klatchat_observer
+docker build -f ../../dockerfiles/Dockerfile.observer -t neon/klatchat_observer:latest .
+# echo "Building Chat Client"
+# docker build -f dockerfiles/Dockerfile.client -t neon/chat_client:latest chat_client
+# echo "Building Chat Server"
+# docker build -f dockerfiles/Dockerfile.server -t neon/chat_server:latest chat_server
+cd ../../scripts
