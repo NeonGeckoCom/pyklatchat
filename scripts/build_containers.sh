c685b12d03877855a5c5e3d873c25239c4f23678
kirill <kirill.grim@gmail.com>
2021-11-29 17:32:46 +0200

    Small fix

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -10,7 +10,7 @@ cp requirements/requirements.txt chat_client
 cp config.py chat_client
 cd chat_client
 docker build -f ../dockerfiles/Dockerfile.client -t neon/chat_client:latest chat_client
-cd ../scripts
+cd ..
 echo "Building Chat Server"
 cp requirements/requirements.txt chat_server
 cp config.py chat_server
