71bc7f368f5538055058614a799984ac62f90c0f
kirill <kirill.grim@gmail.com>
2021-12-01 15:01:16 +0200

    Updated PYTHONPATH

--- chat_server/wsgi.py
+++ chat_server/wsgi.py
@@ -16,10 +16,6 @@
 # Specialized conversational reconveyance options from Conversation Processing Intelligence Corp.
 # US Patents 2008-2021: US7424516, US20140161250, US20140177813, US8638908, US8068604, US8553852, US10530923, US10530924
 # China Patent: CN102017585  -  Europe Patent: EU2156652  -  Patents Pending
-import os.path
-import sys
-
-sys.path.insert(0, os.pardir)
 from .app import create_app
 
 app = create_app()
