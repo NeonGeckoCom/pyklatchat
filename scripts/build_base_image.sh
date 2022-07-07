071a8ea36d331d329c8c668dd76b9be791f90b77
NeonKirill <kirill.grim@gmail.com>
2022-07-07 17:18:21 +0200

    troubleshooting deployment

--- scripts/build_base_image.sh
+++ scripts/build_base_image.sh
@@ -16,7 +16,7 @@ cp requirements/requirements.txt base
 
 cd base
 
-docker build -f ../../dockerfiles/Dockerfile.base -t ghcr.io/neongeckocom/pyklatchat_base:$VERSION .
+docker build -f ../dockerfiles/Dockerfile.base -t ghcr.io/neongeckocom/pyklatchat_base:$VERSION .
 
 cd ../scripts
 echo "Base Image Building Completed Successfully"
