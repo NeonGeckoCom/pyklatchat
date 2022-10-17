f2e19bba22a3d0d4aee5970e394e646fe1656c9b
NeonKirill <kirill.grim@gmail.com>
2022-10-17 23:49:57 +0200

    Fixed up known issues

--- chat_client/static/js/suggestions.js
+++ chat_client/static/js/suggestions.js
@@ -8,13 +8,23 @@ async function renderSuggestions() {
         for (const item of Array.from(items)) {
             importConversationModalSuggestions.insertAdjacentHTML('afterbegin', await buildSuggestionHTML(item['_id'], item['conversation_name']));
         }
-        Array.from(importConversationModalSuggestions.getElementsByClassName('dropdown-item')).forEach(item => {
-            const cid = item.getAttribute('data-cid')
+        Array.from(importConversationModalSuggestions.getElementsByClassName('suggestion-item')).forEach(item => {
+            const cid = item.getAttribute('data-cid');
             if (cid) {
                 item.addEventListener('click', async (e) => {
-                    conversationSearchInput.value = await displayConversation(cid);
+                    await displayConversation(cid);
+                    conversationSearchInput.value = "";
+                    importConversationModal.modal('hide');
+                    importConversationModalSuggestions.innerHTML = "";
+                });
+                item.addEventListener('mouseover', (event) => {
+                   item.classList.add('selected')
+                });
+                item.addEventListener('mouseleave', (event) => {
+                   item.classList.remove('selected')
                 });
             }
         });
+        importConversationModalSuggestions.style.setProperty('display', 'inherit', 'important');
     });
 }
