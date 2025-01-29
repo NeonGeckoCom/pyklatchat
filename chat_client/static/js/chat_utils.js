91ec5ca94613cdaa5cdc03c7b49db8365dee0298
NeonKirill <74428618+NeonKirill@users.noreply.github.com>
2025-01-29 13:04:55 +0100

    Small client UI fix (#152)

--- chat_client/static/js/chat_utils.js
+++ chat_client/static/js/chat_utils.js
@@ -522,7 +522,7 @@ const displayLiveChat = async () => {
         })
         .then(
             async data => {
-                await buildConversation(data, data.skin, true);
+                await buildConversation(data, CONVERSATION_SKINS.PROMPTS, true);
                 return data;
             }
         )
